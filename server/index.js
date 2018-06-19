b97ed525b7aa9c946fee0817e7bd28808d044695
Joie Cosby <jc283108@gmail.com>
2018-06-19 16:45:38 -0600

    broken passport, attempting to fix but we did intall dependecies

--- server/index.js
+++ server/index.js
@@ -2,6 +2,11 @@ const express = require('express');
 const path = require('path');
 const cluster = require('cluster');
 const numCPUs = require('os').cpus().length;
+const bodyParser = require('body-parser');
+const passport = require('passport');
+const config = require('./server/config');
+
+require('./model').connect(config.dbUri);
 
 const PORT = process.env.PORT || 5000;
 
@@ -21,13 +26,30 @@ if (cluster.isMaster) {
 } else {
   const app = express();
 
+
   // Priority serve any static files.
   app.use(express.static(path.resolve(__dirname, '../react-ui/build')));
+
+  app.use(bodyParser.urlencoded({ extended:false }));
+
+  app.use(passport.initialize());
+
+  const localSignupStrategy = require('./passport/local-signup');
+  const localLoginStrategy = require('./passport/local-login');
+  passport.use('local-signup', localSignupStrategy);
+  passport.use('local-login', localLoginStrategy);
+
+  const authCheckMiddleware = require('./server/middleware/auth-check');
+  app.use('/api', authCheckMiddleware);
+
+  const authRoutes = require('./server/routes/auth');
+
+
 
   // Answer API requests.
   app.get('/api', function (req, res) {
     res.set('Content-Type', 'application/json');
-    res.send('{"message":"Hello from the custom server!"}');
+    res.send('{"message":"Hello from the other side!"}');
   });
 
   // All remaining requests return the React app, so it can handle routing.

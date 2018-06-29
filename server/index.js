266ac7372702124abdc90a175eb97e6e6d92ac5d
Kate Stallbaumer <kate.stallbaumer@gmail.com>
2018-06-29 16:04:20 -0600

    deleted clusters from index.js

--- server/index.js
+++ server/index.js
@@ -1,7 +1,5 @@
 const express = require('express');
 const path = require('path');
-const cluster = require('cluster');
-const numCPUs = require('os').cpus().length;
 const bodyParser = require('body-parser');
 const passport = require('passport');
 const config = require('./config');
@@ -11,20 +9,7 @@ require('./model').connect(db);
 
 const PORT = process.env.PORT || 5000;
 console.log(PORT);
-// Multi-process to utilize all CPU cores.
-if (cluster.isMaster) {
-  console.error(`Node cluster master ${process.pid} is running`);
 
-  // Fork workers.
-  for (let i = 0; i < numCPUs; i++) {
-    cluster.fork();
-  }
-
-  cluster.on('exit', (worker, code, signal) => {
-    console.error(`Node cluster worker ${worker.process.pid} exited: code ${code}, signal ${signal}`);
-  });
-
-} else {
   const app = express();
 
 
@@ -63,4 +48,3 @@ if (cluster.isMaster) {
   app.listen(PORT, function () {
     console.error(`Node cluster worker ${process.pid}: listening on port ${PORT}`);
   });
-}

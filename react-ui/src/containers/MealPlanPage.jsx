5ff257ba5e7806936545955896a3af9abd6fb9e5
Tyler Hofer <tylerhofer@Tylers-MacBook-Pro.local>
2018-07-09 10:25:30 -0600

    pushing to test bug

--- react-ui/src/containers/MealPlanPage.jsx
+++ react-ui/src/containers/MealPlanPage.jsx
@@ -58,32 +58,32 @@ class MealPlanPage extends React.Component {
      })
   }
 
-  componentDidMount() {
-    fetch('/api/recipes/' + 'pasta', {
-      method: 'GET',
-      headers: {
-        'Accept' : 'application/json',
-        'Content-Type' : 'application/json',
-        Authorization: `bearer ${Auth.getToken()}`
-      }
-    })
-    .then ( ( res )  => {return res.json()})
-    .then (( data ) => {
-      let tempArr = [];
-      for (var i = 0; i < data.length; i++) {
-        let obj = {
-          image_url: data[i].image_url,
-          recipe_id: data[i].recipe_id,
-          title: data[i].title,
-          expanded: false
-        }
-        tempArr.push(obj);
-     }
-     this.setState({
-       plans:tempArr
-     })
-     })
-  }
+  // componentDidMount() {
+  //   fetch('/api/recipes/' + 'pasta', {
+  //     method: 'GET',
+  //     headers: {
+  //       'Accept' : 'application/json',
+  //       'Content-Type' : 'application/json',
+  //       Authorization: `bearer ${Auth.getToken()}`
+  //     }
+  //   })
+  //   .then ( ( res )  => {return res.json()})
+  //   .then (( data ) => {
+  //     let tempArr = [];
+  //     for (var i = 0; i < data.length; i++) {
+  //       let obj = {
+  //         image_url: data[i].image_url,
+  //         recipe_id: data[i].recipe_id,
+  //         title: data[i].title,
+  //         expanded: false
+  //       }
+  //       tempArr.push(obj);
+  //    }
+  //    this.setState({
+  //      plans:tempArr
+  //    })
+  //    })
+  // }
 
   render() {
     let mealPlans;
